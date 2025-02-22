e3f2b9d279ea189f43e33f6b248a6042d308f663
Vishwa GW <136871744+vishwagw@users.noreply.github.com>
2025-02-22 09:32:25 +0530

    Update trading_bot1.py

--- trading bot model 1/trading_bot1.py
+++ trading bot model 1/trading_bot1.py
@@ -1,1 +1,10 @@
+# import libs;
+import pandas as pd
+import time
+# trad api:
+import aplaca_trade_api as tradeapi
 
+# Alpaca API credentials
+API_KEY = 'YOUR_API_KEY'
+API_SECRET = 'YOUR_API_SECRET'
+BASE_URL = 'https://paper-api.alpaca.markets'
