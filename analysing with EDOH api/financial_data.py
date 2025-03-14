1898075657f9b36e91e54e3e63b177bf3c64f191
Vishwa GW <136871744+vishwagw@users.noreply.github.com>
2025-03-15 01:55:02 +0530

    Create financial_data.py

---
+++ analysing with EDOH api/financial_data.py
@@ -0,0 +1,26 @@
+# loading the financial data:
+import json
+from eodhd import APIClient
+
+class EODHDAPIsDataFetcher:
+    def __init__(self, api_token):
+        self._api_token = api_token
+
+    def fetch_exchanges(self):
+        try:
+            api = APIClient(self._api_token)
+            df= api.get_exchanges()
+            return json.loads(df.to_json(orient="records"))
+        except:
+            print(f"Error fetching data: {e}")
+            return None
+        
+    def fetch_exchange_markets(self, code: str = ""):
+        try:
+            api = APIClient(self._api_token)
+            df = api.get_exchange_symbols(code)
+            return json.loads(df.to_json(orient="records"))
+
+        except Exception as e:
+            print(f"Error fetching data: {e}")
+            return None
